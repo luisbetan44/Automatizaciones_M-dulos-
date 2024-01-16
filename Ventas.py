5c33266c5c78e94af8d2ed19720d592a9aca5273
luisbetan <luis.tacourt@gmail.com>
2024-01-16 16:45:39 -0300

    commit 44

--- Ventas.py
+++ Ventas.py
@@ -1,12 +1,9 @@
 import unittest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.common.by import By
 import xmlrunner
-from selenium.webdriver.support import expected_conditions as EC
-
+import time
+from Elements import find_and_click_element, find_elements, validate_chain_text_xpaht, validate_text, validate_text_by_text, validate_text_visible, validate_text_visible_selector
+from loginhelper import LoginHelper
+from startSession import StartSession
 
 
 
@@ -14,257 +11,165 @@ from selenium.webdriver.support import expected_conditions as EC
 class cuenta_ventas(unittest.TestCase):
     
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r"C:\driverchrome\chromedriver-win64\chromedriver.exe")
-        driver = self.driver
-        driver.implicitly_wait(30)
-        driver.maximize_window()
-        driver.get("https://pwa-portal-staging.silohub.ag/login")
-
-    def test_cuenta_entregas(self):
-        driver = self.driver
-        usermane = driver.find_element_by_id("email")
-        usermane.send_keys("admingd@silohub.ag")
-        usermane.send_keys(Keys.ENTER)
-        time.sleep(3)
-
-
-        passworUser = driver.find_element_by_id("password")
-        passworUser.send_keys("G@viglio123")
-        passworUser.send_keys(Keys.ENTER)
-        time.sleep(3)
-
-        insertButton = driver.find_element_by_xpath("/html/body/app-root/app-login-main/div/div[2]/div/app-login-form/div/div/div[1]/div/div[2]/form/div[4]/app-button/button")
-        insertButton.click()
-        time.sleep(3)
-        
-        ## seleccionar el tenant 
-        selectTenant = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-home-main/div/div[1]/app-tenant-main/app-tenant[8]/div/div/img")
-        selectTenant.click()
-        time.sleep(3)
-
-     # localiza el input y envia el número de la cuenta 
-        input_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "search-options")))
-        input_element.send_keys('1023')
-      
-       #selecciona el elemento oculto y crea un botón para hacer click sobre el 
-        element_to_click = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#search-dropdown > app-accounts-list > ngx-simplebar > div.simplebar-wrapper > div.simplebar-mask > div > div > div > div > div.dropdown-sub-item.accounts-numbers")))
-        driver.execute_script("arguments[0].style.display = 'block';", element_to_click)
-        element_to_click.click()
-        time.sleep(3)
+       
+       
+        self.start_session = StartSession()
+        self.driver = self.start_session.driver
+        # Inicializar la clase LoginHelper
+        self.login_helper = LoginHelper(self.driver)
+   
+   
+    def test_cuenta_ventas(self):
+        # Utilizar métodos de LoginHelper para el inicio de sesión
+        self.login_helper.login("admingd@silohub.ag", "G@viglio123")
+        self.login_helper.select_tenant()
+        self.login_helper.search_and_select_account("1023")
 
         # ingresar al menú de cuentas 
 
-        select_menu_Account = driver.find_element_by_xpath(
-            "/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div[1]/div[3]/div[1]/ngx-simplebar/div[1]/div[2]/div/div/div/ul/li[5]/a/span"
-        )
-        select_menu_Account.click()
+        select_menu_Account = "/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div[1]/div[3]/div[1]/ngx-simplebar/div[1]/div[2]/div/div/div/ul/li[5]/a/span"
+        find_elements(self.driver,select_menu_Account )
 
-        select_sales = driver.find_element_by_xpath(
-            "/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div[1]/div[3]/div[1]/ngx-simplebar/div[1]/div[2]/div/div/div/ul/li[5]/div/ul/li[3]/a"
-        )
-        select_sales.click()
+
+        select_sales = "/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div[1]/div[3]/div[1]/ngx-simplebar/div[1]/div[2]/div/div/div/ul/li[5]/div/ul/li[3]/a"
+        find_elements(self.driver, select_sales )
         time.sleep(3)
 
 
 
         #   Validar titulo de la pantalla 
 
-        title_menu = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/div/span")
-        title_obtained = title_menu.text
+        title_menu = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/div/span"
         tille_expected = "Mis Ventas"
-        self.assertEqual(title_obtained, tille_expected)
-
-        if title_obtained:
-            print("El titulo de la pagina es:", title_menu.text)
-
-
-        else:
-              print("el titulo de la pagina no se enconto ")  
+        validate_text_visible(self.driver, title_menu, tille_expected )  
 
 
         # selecionar botón del filtro
-        selct_filter = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-header-for-responsive-table/div/div/div[2]/div/div[2]/app-filter-button/button/div/span")
-        selct_filter.click()
+        selct_filter = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-header-for-responsive-table/div/div/div[2]/div/div[2]/app-filter-button/button/div/span"
+        find_elements(self.driver,selct_filter )
         time.sleep(2)
  
 
 
         # limpiar  filtro 
 
-        clean_filter = driver.find_element_by_xpath("/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-filter-buttons/div/app-button[1]/button")
-        clean_filter.click()
+        clean_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-filter-buttons/div/app-button[1]/button"
+        find_elements(self.driver,clean_filter )
         time.sleep(2)
 
         # aplicar filtro
 
-        apply_product_filter = driver.find_element_by_xpath("/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-grain-container/div/app-grain-button[1]/div/img")
-        apply_product_filter.click()
+        apply_product_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-grain-container/div/app-grain-button[1]/div/img"
+        find_elements(self.driver,apply_product_filter )
         time.sleep(2)
 
-        apply_Campaign_filter = driver.find_element_by_xpath("/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-season-container/div/app-season-button[3]/div/div")
-        apply_Campaign_filter.click()
+        apply_Campaign_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-season-container/div/app-season-button[3]/div/div"
+        find_elements(self.driver,apply_Campaign_filter )
         time.sleep(2)
 
-        apply_filter = driver.find_element_by_xpath("/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-filter-buttons/div/app-button[2]/button")
-        apply_filter.click()
+        select_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-date-filter/div/app-date-picker/div/input[2]"
+        find_elements(self.driver, select_filter )
+        time.sleep(2)
+
+        arrow_filter1 = "/html/body/div/div[1]/span[1]"
+        amount_click1 = 10
+        find_and_click_element(self.driver, arrow_filter1, amount_click1)
+
+        select_date1 = "/html/body/div/div[2]/div/div[2]/div/span[3]"
+        find_elements(self.driver, select_date1 )
+
+        arrow_filter2 = "/html/body/div/div[1]/span[2]"
+        amount_click2 = 1
+        find_and_click_element(self.driver, arrow_filter2, amount_click2)
+
+        select_date2 = "/html/body/div/div[2]/div/div[2]/div/span[35]"
+        find_elements(self.driver, select_date2 )
+
+        apply_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-filter-buttons/div/app-button[2]/button"
+        find_elements(self.driver,apply_filter )
         time.sleep(2)
 
         # seleccionar dos movimientos 
 
-        select_product_list_1 = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-responsive-table/div/table/tbody/tr[1]/th/input")
-        select_product_list_1.click()
+        select_product_list_1 = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-responsive-table/div/div/table/tbody/tr[1]/th/input"
+        find_elements(self.driver, select_product_list_1 )
         time.sleep(1)
 
 
-        select_product_list_2 = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-responsive-table/div/table/tbody/tr[2]/th/input")
-        select_product_list_2.click()
+        select_product_list_2 = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-responsive-table/div/div/table/tbody/tr[2]/th/input"
+        find_elements(self.driver, select_product_list_2 )
         time.sleep(1)
 
 
 
-        select_product_list_3 = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-responsive-table/div/table/tbody/tr[3]/th/input")
-        select_product_list_3.click()
+        select_product_list_3 = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-responsive-table/div/div/table/tbody/tr[3]/th/input"
+        find_elements(self.driver, select_product_list_3 )
         time.sleep(1)
 
         # selecionar el botón para descargar los movimientos 
 
-        button_download = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-header-for-responsive-table/div/div/div[2]/div/div[1]/app-download-button/div/button[2]")
-        button_download.click()
+        button_download = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-header-for-responsive-table/div/div/div[2]/div/div[1]/app-download-button/div/button[2]"
+        find_elements(self.driver, button_download )
         time.sleep(1)
 
-        select_format = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-header-for-responsive-table/div/div/div[2]/div/div[1]/app-download-button/div/ul/li[2]/a")
-        select_format.click()
+        select_format = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-header-for-responsive-table/div/div/div[2]/div/div[1]/app-download-button/div/ul/li[2]/a"
+        find_elements(self.driver, select_format )
         time.sleep(3)
 
-        # ingresar al detalle de una venta 
+        # ingresar al detalle de la tercera venta 
          
-        insert_detail = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-responsive-table/div/table/tbody/tr[3]/td[3]/span/span")
-        insert_detail.click()
+        insert_detail = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-sales/app-sales-shared/app-responsive-table/div/div/table/tbody/tr[3]/td[1]"
+        find_elements(self.driver, insert_detail )
         time.sleep(3)
 
         # validar número de venta 
 
-        number_sale = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/app-header-for-detail/div[1]/div")
-        number_sale_obtained = number_sale.text
-        number_sale_expected = "Venta EP 0001 00111822"
-        self.assertEqual(number_sale_obtained, number_sale_expected)
-
-        if number_sale_obtained:
-            print("El número de venta es:", number_sale.text)
-
-
-        else:
-              print("El número de venta no es correcto ")  
+        number_sale = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/app-header-for-detail/div[1]/div"
+        number_sale_expected = "Venta F 0001 00118245"
+        validate_text_visible(self.driver, number_sale, number_sale_expected ) 
 
         # validar cantidad de kilos 
 
-        amount_kilos = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/app-header-for-detail/div[2]/div/div[2]/div[2]")
-        amount_kilos_obtained = amount_kilos.text
-        amount_kilos_expected = ["337.005,00 Kg", "337,01 Tn", "3.370,05 QQ"]
+        amount_kilos = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/app-header-for-detail/div[2]/div/div[2]/div[2]"
+        amount_kilos_expected = ["150.000,00 Kg", "150,00 Tn", "1.500,00 QQ"]
+        validate_chain_text_xpaht(self.driver, amount_kilos, amount_kilos_expected)
         
-        if amount_kilos_obtained in amount_kilos_expected:
-            print("La cantidad de kilos es:", amount_kilos_obtained)
-        else:
-            print("La cantidad de kilos no es la correcta")
         # validar producto
      
-        type_product = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/app-header-for-detail/div[2]/div/div[2]/div[3]")
-        type_product_obtained = type_product.text
+        type_product = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/app-header-for-detail/div[2]/div/div[2]/div[3]"
         type_product_expected = "De Soja"
-        self.assertEqual(type_product_obtained, type_product_expected)
-
-        if type_product_obtained:
-            print("El tipo de producto es:", type_product.text)
-
-
-        else:
-              print("El tipo de producto no es correcto ")  
-
-       
+        validate_text_visible(self.driver,type_product, type_product_expected ) 
      # validar datos de la venta 
 
-        title_data = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/section/div/h2[1]")
-        title_data_obtained = title_data.text
+        title_data = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/section/div/h2[1]"
         title_data_expected = "DATOS DE LA VENTA"
-        self.assertEqual(title_data_obtained, title_data_expected)
+        validate_text_visible(self.driver,title_data, title_data_expected )   
 
-        if title_data_obtained:
-            print("El titulo de la segunda card  es:", title_data.text)
+    # Validar fecha, campaña, mercado, fecha de vencimiento, numero de contrato  
 
+        date_data = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/section/div/div[1]/div[1]"
+        date_data_expected = "29/03/2023"
+        validate_text_visible(self.driver, date_data, date_data_expected )
 
-
-        else:
-              print("El tutilo de la segunda card no es correcto ")  
-
-       
-
-      # Validar fecha, campaña, mercado, fecha de vencimiento, numero de contrato  
-
-        date_data = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/section/div/div[1]/div[1]")
-        date_data_obtained = date_data.text
-        date_data_expected = "07/03/2022"
-        self.assertEqual(date_data_obtained, date_data_expected)
-
-        if date_data_obtained:
-            print("La fecha de la venta  es:", date_data.text)
+        campaign_sale = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/section/div/div[1]/div[2]"
+        campaign_sale_expected = "21/22"
+        validate_text_visible(self.driver, campaign_sale, campaign_sale_expected )
 
 
 
-        else:
-              print("La fecha de la venta no es correcta ")  
-
-
-        campaign_sale = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/section/div/div[1]/div[2]")
-        campaign_sale_obtained = campaign_sale.text
-        campaign_sale_expected = "20/21"
-        self.assertEqual(campaign_sale_obtained, campaign_sale_expected)
-
-        if campaign_sale_obtained:
-            print("La campaña de la venta  es:", campaign_sale.text)
-
-
-
-        else:
-              print("La campaña de la venta no es correcta ")
-
-
-
-        texto_objetivo = "RENOVA - Soja  - Timbues"
-
-
-        elemento = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, f"//*[contains(text(), '{texto_objetivo}')]"))
-        )
-
-
-        if elemento.is_displayed():
-           print("El texto del tipo de mercado es visible en la página.")
-        else:
-           print("El texto del tipo de mercado no es visible en la página.")
-
-
-        print("El tipo de mercado es:")
-        print(elemento.text)
+        expected_text = "03 Playa S. Miguel (T6 y M. Pampa)"
+        validate_text_by_text(self.driver, expected_text)
        
 
         
 
-        date_expiration = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/section/div/div[1]/div[4]")
-        date_expiration_obtained = date_expiration.text
-        date_expiration_expected = "07/10/2021"
-        self.assertEqual(date_expiration_obtained, date_expiration_expected)
-
-        if date_expiration_obtained:
-            print("La fecha de vencimiento es:", date_expiration.text)
-
-
-
-        else:
-              print("La feccha de vencimiento  no es correcta ")  
+        date_expiration = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-sales/div[1]/section/div/div[1]/div[4]"
+        date_expiration_expected = "24/08/2022"
+        validate_text_visible(self.driver, date_expiration, date_expiration_expected )  
 
         
-        go_out_list = driver.find_element_by_xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/app-header-for-screen/div/div/div/a")
-        go_out_list.click()
+        go_out_list = "/html/body/app-root/app-layout/app-vertical/div/div/div/app-header-for-screen/div/div/div/a"
+        find_elements(self.driver, go_out_list )
         time.sleep(2)
 
         
