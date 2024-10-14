1b046bbe85b6daedbbc45b24a2c3507ba9860286
luisbetan44 <luis@silohub.ag>
2024-10-14 13:05:36 -0300

    14/10/24 13:05

--- EntregasFacturados.py
+++ EntregasFacturados.py
@@ -1,14 +1,15 @@
 import unittest
 import xmlrunner
 import time
-from Elements import find_and_click_element, find_elements, validate_chain_text_xpaht, validate_text, validate_text_visible, validate_text_visible_selector
+from Elements import calendar_todate_retro, find_elements, find_elements_id, validate_character_numeric_element, validate_text, validate_text_visible
+from Elements2 import validate_character_string_element
 from loginhelper import LoginHelper
 from startSession import StartSession
 
 
 
 
-class cuenta_entregas(unittest.TestCase):
+class entregas_facturados(unittest.TestCase):
     
     def setUp(self):
        
@@ -19,7 +20,7 @@ class cuenta_entregas(unittest.TestCase):
         self.login_helper = LoginHelper(self.driver)
    
    
-    def test_cuenta_entregas(self):
+    def test_delivery_applied(self):
         # Utilizar métodos de LoginHelper para el inicio de sesión
         self.login_helper.login("admingd@silohub.ag", "G@viglio123")
         self.login_helper.select_tenant()
@@ -42,44 +43,41 @@ class cuenta_entregas(unittest.TestCase):
         validate_text(self.driver, title_menu, tille_expected )
 
         # selecionar botón del filtro
-        selct_filter = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-deliveries/app-deliveries-shared/app-header-for-responsive-table/div/div/div[2]/div/div[2]/app-filter-button/button/div/i"
+        selct_filter = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-deliveries/app-deliveries-shared/app-header-for-responsive-table/div/div/div[2]/div/div/app-filter-button/button/div/span"
         find_elements(self.driver, selct_filter )
         time.sleep(3)
  
         # limpiar  filtro 
 
-        clean_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-filter-buttons/div/app-button[1]/button"
+        clean_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-tag-container/div/div/div[7]/app-tag/div/div/i"
         find_elements(self.driver, clean_filter )
-        time.sleep(3)
+        time.sleep(4)
 
-        # aplicar filtro
+      
 
-        apply_product_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-grain-container/div/app-grain-button[2]/div/img"
+        # aplicar filtro soja 2324  rango de seis meses 
+
+        apply_product_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-grain-container/div/app-grain-button[1]/div/img"
         find_elements(self.driver, apply_product_filter )
         time.sleep(3)
 
-        apply_Campaign_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-season-container/div/app-season-button[2]/div/div"
+        apply_Campaign_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-season-container/div/app-season-button[1]/div/div"
         find_elements(self.driver, apply_Campaign_filter )
         time.sleep(3)
+
+        apply_state_filter = "Facturados"
+        find_elements_id(self.driver, apply_state_filter )
+        time.sleep(3)
        
-        select_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-date-filter/div/app-date-picker/div/input[2]"
-        find_elements(self.driver, select_filter )
+        # aplicar filtro de fecha actual a seis meses para atras 
+
+        select_calendar = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-date-filter/div/app-date-picker/div/input[2]"
+        popup_xpath = "//div[contains(@class, 'flatpickr-calendar')]"
+        select_chevron = "//span[@class='flatpickr-prev-month']"
+        popup_xpath2 = "//div[contains(@class, 'flatpickr-calendar')]"
+        click_chevron = 6
+        calendar_todate_retro(self.driver, select_calendar, popup_xpath, select_chevron, popup_xpath2, clicks=click_chevron)
         time.sleep(2)
-
-        arrow_filter1 = "/html/body/div/div[1]/span[1]"
-        amount_click1 = 12
-        find_and_click_element(self.driver, arrow_filter1, amount_click1)
-
-        select_date1 = "/html/body/div/div[2]/div/div[2]/div/span[3]"
-        find_elements(self.driver, select_date1 )
-
-        arrow_filter2 = "/html/body/div/div[1]/span[2]"
-        amount_click2 = 1
-        find_and_click_element(self.driver, arrow_filter2, amount_click2)
-
-        select_date2 = "/html/body/div/div[2]/div/div[2]/div/span[35]"
-        find_elements(self.driver, select_date2 )
-
         apply_filter = "/html/body/ngb-offcanvas-panel/div/ngx-simplebar/div[1]/div[2]/div/div/div/app-filter-content/div[2]/app-filter-buttons/div/app-button[2]/button"
         find_elements(self.driver, apply_filter )
         time.sleep(3)
@@ -91,13 +89,11 @@ class cuenta_entregas(unittest.TestCase):
 
 
         tn_bruto = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-deliveries/app-deliveries-shared/app-header-for-responsive-table/div/div/div[1]/div/div[1]/app-totalizer/div/div/div[2]/div[2]/span[1]'
-        tn_bruto_expected = ["231,36","2.313,60","231.360,00"]
-        validate_chain_text_xpaht(self.driver, tn_bruto, tn_bruto_expected )  
+        validate_character_numeric_element(self.driver, tn_bruto)  
 
 
         tn_netos = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-deliveries/app-deliveries-shared/app-header-for-responsive-table/div/div/div[1]/div/div[2]/app-totalizer/div/div/div[2]/div[2]/span[1]'
-        tn_netos_expected = ["391,06", "3.910,60", "391.060,00"]
-        validate_chain_text_xpaht(self.driver, tn_netos, tn_netos_expected )  
+        validate_character_numeric_element(self.driver, tn_netos )  
 
         # seleccionar varios movimientos del listado 
         
@@ -134,54 +130,46 @@ class cuenta_entregas(unittest.TestCase):
 
         # ingresar al detalle del tercer movimiento
 
-        insert_movemenst = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-deliveries/app-deliveries-shared/app-responsive-table/div/div[2]/table/tbody/tr[3]/td[2]/span/span"
+        insert_movemenst = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-deliveries/app-deliveries-shared/app-responsive-table/div/div[2]/table/tbody/tr[3]/td[3]/span/div"
         find_elements(self.driver, insert_movemenst )
         time.sleep(3)
 
         # validar numero de Tk 
 
         number_TK = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-header-for-detail/div[1]/div'
-        number_TK_expected = "Entrega TK 0001 00124125"
-        validate_text_visible(self.driver, number_TK, number_TK_expected )
+        validate_character_string_element(self.driver, number_TK )
 
         # validar el total de toneladas de la entrega 
 
         tn_delivery = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-header-for-detail/div[2]/div/div[2]/div[1]'
-        tn_delivery_expected = ["37,04 Tn", "370,40 QQ", "37.040,00 Kg"]
-        validate_chain_text_xpaht(self.driver, tn_delivery, tn_delivery_expected ) 
+        validate_character_string_element(self.driver, tn_delivery) 
 
         # validar producto
 
         type_product = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-header-for-detail/div[2]/div/div[2]/div[2]'
-        type_product_expected = "De Maiz"
-        validate_text_visible(self.driver, type_product, type_product_expected  )  
+        validate_character_string_element(self.driver, type_product  )  
 
 
         # Validar  fecha campaña cuenta campo 
 
         title_delivery = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-detail-table/div/div/div[1]/div[1]/span'
-        title_delivery_expected = "DATOS DE LA ENTREGA"
-        validate_text_visible(self.driver, title_delivery, title_delivery_expected  )
+        validate_character_string_element(self.driver, title_delivery)
         
         date_delivery = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-detail-table/div/div/div[1]/div[2]/div[1]/div[2]'
-        date_delivery_expected = "27/04/2023"
-        validate_text_visible(self.driver, date_delivery, date_delivery_expected  ) 
+        validate_character_string_element(self.driver, date_delivery ) 
 
         campaign_delivery = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-detail-table/div/div/div[1]/div[2]/div[2]/div[2]'
-        campaign_delivery_expected = "22/23"
-        validate_text_visible(self.driver, campaign_delivery, campaign_delivery_expected  ) 
+        validate_character_string_element(self.driver, campaign_delivery ) 
 
         business_name = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-detail-table/div/div/div[1]/div[2]/div[3]/div[2]'
-        business_name_expected = "JUAN DEMO"
-        validate_text_visible(self.driver, business_name, business_name_expected  )  
+        validate_character_string_element(self.driver, business_name)  
  
         field_business = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-detail-table/div/div/div[1]/div[2]/div[4]/div[2]'
-        field_business_expected = "1399 LA MORENITA"
-        validate_text_visible(self.driver, field_business,  field_business_expected  ) 
+        validate_character_string_element(self.driver, field_business) 
 
         # ingresar al analisis 
 
-        into_analysis = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-detail-table/div/div/div[2]/div[1]/div/button[1]"
+        into_analysis = "/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-detail-deliveries/div/app-detail-table/div/div/div[2]/div[1]/div/button[2]"
         find_elements(self.driver, into_analysis )
         time.sleep(5)
 
@@ -192,13 +180,8 @@ class cuenta_entregas(unittest.TestCase):
         validate_text_visible(self.driver, title_analysis,  title_analysis_expected  ) 
   
 
-        data_analysis = '#layout-wrapper > div > div > div > app-quality-analisis > app-responsive-table > div > div.table-responsive > table > tbody > tr:nth-child(9) > td.text-nowrap.align-middle.f-size-12.ellipsis-cell.fw-bold > span > span'
-        data_analysis_expected = "Peso Hectolítrico"
-        validate_text_visible_selector(self.driver, data_analysis,  data_analysis_expected  )  
-
-        foreign_matter = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-quality-analisis/app-responsive-table/div/div[2]/table/tbody/tr[9]/td[2]/span/span'
-        foreign_matter_expected = "72%"
-        validate_text_visible(self.driver, foreign_matter,  foreign_matter_expected  ) 
+        foreign_matter = '/html/body/app-root/app-layout/app-vertical/div/div/div/div/app-quality-analisis/app-responsive-table/div/div[2]/table/tbody/tr[9]/td[2]/span/div/span'
+        validate_character_string_element(self.driver, foreign_matter ) 
 
 
         # salir al listado 
@@ -218,6 +201,9 @@ class cuenta_entregas(unittest.TestCase):
 
 
 
+
+
+
     def tearDown(self):
         self.driver.close()
 
@@ -226,7 +212,7 @@ class cuenta_entregas(unittest.TestCase):
 
 
 if __name__ == "__main__":
-  test_suite = unittest.TestLoader().loadTestsFromTestCase(cuenta_entregas)
+  test_suite = unittest.TestLoader().loadTestsFromTestCase(entregas_facturados)
   runner = xmlrunner.XMLTestRunner(output='reportCuentaEntregas')
   runner.run(test_suite)
    
